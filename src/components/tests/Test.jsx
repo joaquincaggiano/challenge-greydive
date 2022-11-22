f04869f97752383550353629100649ea2ba13dc5
Joaquin Caggiano <joaquincaggiano@gmail.com>
2022-11-22 20:24:06 -0300

    using hooks

--- src/components/tests/Test.jsx
+++ src/components/tests/Test.jsx
@@ -1,5 +1,5 @@
 // React Hooks
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 
 // Database
 import greydivedb from "../../database/greydivedb.json";
@@ -10,9 +10,6 @@ import { useParams } from "react-router-dom";
 // Css
 import styles from "../../css/Test.module.css";
 
-// React player
-import ReactPlayer from "react-player";
-
 // Bootstrap
 import Card from "react-bootstrap/Card";
 
@@ -21,9 +18,20 @@ export const Test = () => {
 
   const headerRef = useRef();
 
-  const findTest = greydivedb.find((oneTest) => {
-    return oneTest.cliente === params.id;
-  });
+  const [oneTest, setOneTest] = useState();
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    setisLoading(true);
+    if (greydivedb.length !== 0) {
+      const findTest = greydivedb.find((test) => {
+        return test.cliente === params.id;
+      });
+      // console.log("findTest", findTest)
+      setOneTest(findTest);
+      setisLoading(false);
+    }
+  }, []);
 
   // Convertir string con la primera letra Mayúscula
   const toUpperCaseString = (string) => {
@@ -35,65 +43,69 @@ export const Test = () => {
     headerRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
-  console.log("Cliente", findTest);
+  console.log("Cliente", oneTest);
 
   return (
-    <div className={styles.boxContain} ref={headerRef}>
-      <div className={styles.upArrow} onClick={upScrollHandler}>
-        <i className="fa-solid fa-arrow-up"></i>
-      </div>
+    <>
+      {!isLoading && (
+        <div className={styles.boxContain} ref={headerRef}>
+          <div className={styles.upArrow} onClick={upScrollHandler}>
+            <i className="fa-solid fa-arrow-up"></i>
+          </div>
 
-      <h1 className="text-center fw-bold my-3">TEST</h1>
-      <h2>
-        <b>Cliente:</b> {toUpperCaseString(findTest.cliente)}
-      </h2>
+          <h1 className="text-center fw-bold my-3">TEST</h1>
+          <h2>
+            <b>Cliente:</b> {toUpperCaseString(oneTest.cliente)}
+          </h2>
 
-      <ReactPlayer
-        url={findTest?.linkVideo}
-        controls
-        loop
-        width="100%"
-        height="100%"
-      />
+          <video
+            src={oneTest.linkVideo}
+            width="100%"
+            height="100%"
+            controls
+            type="video/mp4"
+          />
 
-      {/* <video src={findTest.linkVideo} loop /> */}
+          <h3 className={styles.timeClass}>
+            Tiempo del Test:{" "}
+            <p className="d-inline text-white">{oneTest.timeTest}</p>
+          </h3>
 
-      <h3 className={styles.timeClass}>
-        Tiempo del Test:{" "}
-        <p className="d-inline text-white">{findTest.timeTest}</p>
-      </h3>
+          <h2 className="mt-5 mb-md-4 text-center fw-bold fs-1">
+            Transcripción
+          </h2>
+          <div className={styles.transcripcionClass}>
+            <p className="fs-4">{toUpperCaseString(oneTest.transcripcion)}</p>
+          </div>
 
-      <h2 className="mt-5 mb-md-4 text-center fw-bold fs-1">Transcripción</h2>
-      <div className={styles.transcripcionClass}>
-        <p className="fs-4">{toUpperCaseString(findTest.transcripcion)}</p>
-      </div>
-
-      <h2 className="mt-5 text-center fw-bold fs-1">Tareas</h2>
-      <div className="text-lg-center">
-        <p className="fs-4 p-2">
-          <b className="fs-3">Escenario:</b> {findTest.escenario}
-        </p>
-      </div>
-      <div className="container">
-        <div className="row align-items-center">
-          {findTest.preguntas.map((oneQuestion, i) => {
-            return (
-              <div key={i} className="col-sm-12 col-md-6 col-lg-4">
-                <h3>Tarea {i + 1}:</h3>
-                <Card className={styles.cardStyle}>
-                  <Card.Body>
-                    <Card.Title>{oneQuestion.texto}</Card.Title>
-                    <Card.Text className={`${styles.timeClass} mt-4`}>
-                      Duración de la tarea:{" "}
-                      <b className="text-white">{oneQuestion.tiempo}</b>
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </div>
-            );
-          })}
+          <h2 className="mt-5 text-center fw-bold fs-1">Tareas</h2>
+          <div className="text-lg-center">
+            <p className="fs-4 p-2">
+              <b className="fs-3">Escenario:</b> {oneTest.escenario}
+            </p>
+          </div>
+          <div className="container">
+            <div className="row align-items-center">
+              {oneTest.preguntas.map((oneQuestion, i) => {
+                return (
+                  <div key={i} className="col-sm-12 col-md-6 col-lg-4">
+                    <h3>Tarea {i + 1}:</h3>
+                    <Card className={styles.cardStyle}>
+                      <Card.Body>
+                        <Card.Title>{oneQuestion.texto}</Card.Title>
+                        <Card.Text className={`${styles.timeClass} mt-4`}>
+                          Duración de la tarea:{" "}
+                          <b className="text-white">{oneQuestion.tiempo}</b>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
--- src/components/tests/TestList.jsx
+++ src/components/tests/TestList.jsx
@@ -1,3 +1,6 @@
+// React Hooks
+import { useState, useEffect } from "react";
+
 // Database
 import greydivedb from "../../database/greydivedb.json";
 
@@ -10,30 +13,45 @@ import styles from "../../css/TestList.module.css";
 export const TestList = () => {
   const navigate = useNavigate();
 
-  const testsData = greydivedb.map((oneData) => {
-    return {
-      name: oneData.cliente,
-      escenario: oneData.escenario,
-    };
-  });
-  console.log("TEST", testsData);
+  const [dataTest, setDataTest] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    setisLoading(true);
+    if (greydivedb.length !== 0) {
+      const testsData = greydivedb.map((oneData) => {
+        return {
+          name: oneData.cliente,
+          escenario: oneData.escenario,
+        };
+      });
+      setDataTest(testsData);
+      setisLoading(false);
+    }
+  }, []);
+
+  // console.log("TEST", dataTest);
 
   return (
-    <div className={styles.testBox}>
-      {testsData.map((oneTest, i) => (
-        <div
-          className={styles.card}
-          key={i}
-          onClick={() => navigate(`/${oneTest.name}`)}
-        >
-          <div className={styles.front}>
-            <h2>{oneTest.name[0].toUpperCase() + oneTest.name.slice(1)}</h2>
-          </div>
-          <div className={styles.back}>
-            <p>{oneTest.escenario}</p>
-          </div>
+    <>
+      {!isLoading && (
+        <div className={styles.testBox}>
+          {dataTest.map((oneTest, i) => (
+            <div
+              className={styles.card}
+              key={i}
+              onClick={() => navigate(`/${oneTest.name}`)}
+            >
+              <div className={styles.front}>
+                <h2>{oneTest.name[0].toUpperCase() + oneTest.name.slice(1)}</h2>
+              </div>
+              <div className={styles.back}>
+                <p>{oneTest.escenario}</p>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 };
